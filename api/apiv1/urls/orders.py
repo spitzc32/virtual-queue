3e366f0f1dee50ac9bc71f7acd763e84ade3b3d0
kibalias <74294078+kibalias@users.noreply.github.com>
2021-05-26 18:30:21 +0800

    Updated orders and tasks urls

--- api/apiv1/urls/orders.py
+++ api/apiv1/urls/orders.py
@@ -4,9 +4,21 @@ from django.urls import re_path
 # TODO: import the made View Classes and fill out the order patterns
 # After that, import this in __init__.py for us to connect it in our
 # main url (@Jan)
+from ..views.orders import (
+	OrderRetrieveAPIView,
+	OrderDetailView
+	)
+
 
 order_patterns = [
-	
+	re_path(
+		'list/', 
+		OrderRetrieveAPIView.as_view(),
+	),
+	re_path(
+		r'^(?P<pk>\d+)/details', 
+		OrderDetailView.as_view(),
+	),
 ]
 
 order_patterns = format_suffix_patterns(order_patterns)
--- api/apiv1/urls/tasks.py
+++ api/apiv1/urls/tasks.py
@@ -4,9 +4,21 @@ from django.urls import re_path
 # TODO: import the made View Classes and fill out the order patterns
 # After that, import this in __init__.py for us to connect it in our
 # main url (@Jan)
+from ..views.tasks import (
+	StoreTaskRetrieveAPIView,
+	StoreTaskDetailView
+	)
+
 
 tasks_patterns = [
-	
+	re_path(
+		'list/', 
+		StoreTaskRetrieveAPIView.as_view(),
+	),
+	re_path(
+		r'^(?P<pk>\d+)/details', 
+		StoreTaskDetailView.as_view(),
+	),
 ]
 
 tasks_patterns = format_suffix_patterns(tasks_patterns)
