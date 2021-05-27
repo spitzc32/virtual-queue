457f88e24540e957fe30a6d4c432cf06c4213775
kibalias <74294078+kibalias@users.noreply.github.com>
2021-05-27 18:06:37 +0800

    initial commit of __init__.py

--- api/apiv1/urls/__init__.py
+++ api/apiv1/urls/__init__.py
@@ -2,14 +2,18 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path, include
 
 from .accounts import account_patterns
+from .orders import order_patterns
+from .tasks import tasks_patterns
 
 app_name='api_v1'
 
 urlpatterns = [
 	path(
 		'account/', 
-		include(account_patterns)
+		include(account_patterns),
+		'order/', 
+		include(order_patterns),
+		'tasks/', 
+		include(tasks_patterns)
 	),
 ]
-
-
